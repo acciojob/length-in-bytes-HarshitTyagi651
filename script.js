a7a22efd26625274cb1e2c2a62f782dc18208075
HarshitTyagi651 <154967440+HarshitTyagi651@users.noreply.github.com>
2024-08-13 13:53:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length
+	let size = 0;
+	for(let i=0; i<str.length; i++){
+		const code = str.charCodeAt(i);
+		if (code<=0x007F) {
+			size++
+		}
+		else if (code<=0x07FF) {
+			size+=2
+		}
+		else if (code<=0xFFFF) {
+			size=+3
+		}
+		else{
+			size+=4
+		}
+	}
+	return size
 };
 
 // // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
  
